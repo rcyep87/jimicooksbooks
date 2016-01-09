50e64f1fb5e40a4758480e30ae71f3394a44dc9d
Ryan Yep <ryan.yep@gmail.com>
2016-01-09 14:13:11 -0600

    Add user foreign key to accounts table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160109180753) do
+ActiveRecord::Schema.define(version: 20160109200937) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20160109180753) do
     t.integer  "acct_number"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
 
   create_table "payees", force: :cascade do |t|
