459551a1e45e5e25cf1e49d40bb0b47d5d0dca40
Ryan Yep <ryan.yep@gmail.com>
2016-01-09 12:53:02 -0600

    Fixed relations for models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :payees
+  has_many :accounts
+  has_many :transactions, through: :accounts
 end
