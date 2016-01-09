16abb796e417ae69f22203afc0c89058daddc7c7
Ryan Yep <ryan.yep@gmail.com>
2016-01-09 12:54:08 -0600

    Migrated with models and tables updated

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,44 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160109172006) do
+ActiveRecord::Schema.define(version: 20160109180753) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "accounts", force: :cascade do |t|
+    t.string   "type"
+    t.float    "balance"
+    t.integer  "acct_number"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "payees", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "user_id"
+    t.integer  "account_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "payees", ["account_id"], name: "index_payees_on_account_id", using: :btree
+  add_index "payees", ["user_id"], name: "index_payees_on_user_id", using: :btree
+
+  create_table "transactions", force: :cascade do |t|
+    t.date     "date"
+    t.float    "credit"
+    t.float    "debit"
+    t.integer  "user_id"
+    t.integer  "account_id"
+    t.integer  "payee_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "transactions", ["account_id"], name: "index_transactions_on_account_id", using: :btree
+  add_index "transactions", ["payee_id"], name: "index_transactions_on_payee_id", using: :btree
+  add_index "transactions", ["user_id"], name: "index_transactions_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -34,4 +68,9 @@ ActiveRecord::Schema.define(version: 20160109172006) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "payees", "accounts"
+  add_foreign_key "payees", "users"
+  add_foreign_key "transactions", "accounts"
+  add_foreign_key "transactions", "payees"
+  add_foreign_key "transactions", "users"
 end
