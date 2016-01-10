03c173653c8900b1ca7e6de2faf900e0ee54661e
Michelle Dotzenrod <dotzenrodm@gmail.com>
2016-01-09 19:53:19 -0600

    number must be 0 or greater than, credit/debit

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,20 +1,19 @@
 class Transaction < ActiveRecord::Base
   belongs_to :account
   belongs_to :payee
-  before_save :check_only_one_entry
   before_save :negate_debit_entry
+  before_save :check_only_one_entry
 
 
+  validates :credit, :numericality => { :greater_than_or_equal_to => 0, :allow_blank => true }
+  validates :debit, :numericality => { :greater_than_or_equal_to => 0, :allow_blank => true }
 
-  # validates :debit, :numericality => { :less_than_or_equal_to => 0 }
-  validates :credit, :numericality => { :greater_than => 0, :allow_blank => true }
-
-  #validate that there is either a debit or credit, for each instance, not both
 
   def negate_debit_entry
     self.debit = -(self.debit) unless self.debit.nil?
   end
 
+  #validate that there is either a debit or credit, for each instance, not both
   def check_only_one_entry
     if self.credit != nil && self.debit != nil
       false
